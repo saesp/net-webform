095a8e60aaa86d2ec505ff7d4cfb8f692b12e271
Salvatore Esposito <salvatore.esposito@atos.net>
2023-07-06 12:29:02 +0200

    migrations

---
+++ net-webform/Migrations/202307060948541_create.cs
@@ -0,0 +1,45 @@
+﻿namespace net_webform.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Systems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        OrderDate = c.DateTime(nullable: false),
+                        InsertionDate = c.DateTime(nullable: false),
+                        ChangedDate = c.DateTime(nullable: false),
+                        Notes = c.String(nullable: false, maxLength: 1000),
+                        TypologyId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Typologies", t => t.TypologyId)
+                .Index(t => t.TypologyId);
+            
+            CreateTable(
+                "dbo.Typologies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Systems", "TypologyId", "dbo.Typologies");
+            DropIndex("dbo.Systems", new[] { "TypologyId" });
+            DropTable("dbo.Typologies");
+            DropTable("dbo.Systems");
+        }
+    }
+}
---
+++ net-webform/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace net_webform.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<webapp.Models.SystemContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(webapp.Models.SystemContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
